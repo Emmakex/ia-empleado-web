2b52dd2b09a6e0199553fd07f215d5cf9ba25384
Edu <133777880+Emmakex@users.noreply.github.com>
2026-09-12 04:49:18 +0200

    fix: load mobile navigation styles in Spanish layout

--- app/(es)/layout.tsx
+++ app/(es)/layout.tsx
@@ -3,6 +3,7 @@ import "../globals.css";
 import "../light-theme.css";
 import "../employee-content.css";
 import "../employee-discovery.css";
+import "../mobile-navigation.css";
 import { getDictionary } from "../../lib/i18n";
 
 const dictionary = getDictionary("es");
