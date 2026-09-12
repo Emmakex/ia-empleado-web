d3f8eebf689513de59eb87ce23081ed5750291c8
Edu <133777880+Emmakex@users.noreply.github.com>
2026-09-12 15:44:23 +0200

    feat: load department and integration branding styles

--- app/(es)/layout.tsx
+++ app/(es)/layout.tsx
@@ -16,6 +16,7 @@ import "../brand-system.css";
 import "../brand-fidelity.css";
 import "../brand-content.css";
 import "../brand-sector-content.css";
+import "../brand-organization-content.css";
 import { getDictionary } from "../../lib/i18n";
 
 const dictionary = getDictionary("es");
