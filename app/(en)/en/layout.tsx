1642dab0c0ee3f72609932bec816970ce7c1aab5
Edu <133777880+Emmakex@users.noreply.github.com>
2026-09-12 14:43:40 +0200

    fix: load canonical brand fidelity overrides

--- app/(en)/en/layout.tsx
+++ app/(en)/en/layout.tsx
@@ -13,6 +13,7 @@ import "../../comparison-content.css";
 import "../../sector-use-cases.css";
 import "../../organization-map.css";
 import "../../brand-system.css";
+import "../../brand-fidelity.css";
 import { getDictionary } from "../../../lib/i18n";
 
 const dictionary = getDictionary("en");
